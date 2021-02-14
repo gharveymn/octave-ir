2114f5f50ba8b1c06a671cf5be732aa308193472
Gene Harvey <gharveymn@gmail.com>
2021-02-14 11:02:20 -0600

    save

---
+++ source/include/ir-structure-fwd.hpp
@@ -0,0 +1,19 @@
+/** ir-structure-fwd.hpp
+ * Copyright © 2021 Gene Harvey
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef OCTAVE_IR_IR_STRUCTURE_FWD_HPP
+#define OCTAVE_IR_IR_STRUCTURE_FWD_HPP
+
+class ir_structure;
+class ir_substructure;
+
+class ir_component_fork;
+class ir_component_loop;
+class ir_component_sequence;
+class ir_function;
+
+#endif // OCTAVE_IR_IR_STRUCTURE_FWD_HPP
